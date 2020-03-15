5aa29248cddcececf8d277648f7e80c781261c1c
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-15 11:42:43 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,18 +1,2 @@
-class Shoe
-  def initialize(brand="Nike")
-    @brand=brand
- end
-end
 
-def brand
-  @brand
-end
-
-def shoe_color=(shoe_colorcolor)
-  @shoe_color=shoe_color
-end
-
-def shoe_color
-  @shoe_color
-end
   
